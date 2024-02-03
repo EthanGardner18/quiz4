c7f72e915c33671f398561329f186fa3366b7ba0
EthanGardner18 <ethangrdnr@gmail.com>
2024-02-03 00:48:16 -0600

    question2 done

--- 2.py
+++
@@ -1,6 +0,0 @@
-# WAP to demonstrate ABC. Create a base class and inside of this base class there
-# should be two abstract methods (use @bastractmethod decorator). This base class
-# should be inherited by two other classes which will implement the methods inside of
-# the base class. Come up with your own classes. Please do not create a class called
-# vehicle and inherit it into motorbike and car.
-# [Name of the file should be abc_class.py](1point)
