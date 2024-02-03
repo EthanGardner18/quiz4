cd4797426ac413a9dd84b5bf4165edfb83936716
EthanGardner18 <ethangrdnr@gmail.com>
2024-02-03 00:08:57 -0600

    question1 done

--- 1.py
+++
@@ -1,5 +0,0 @@
-# Write a program(WAP) in which you will be using a python argparse module to parse
-# the input arguments. There will be three inputs 1) string 2) int 3) float
-# Example >>python3 arguments.py hello 007 9.11
-# [Name of the file should be argumnets.py](1point)
-
