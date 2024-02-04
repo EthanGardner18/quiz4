f3c9fde0916854ab478a2a6cba12892f35f627f0
Ethan Gardner <etgardn@siue.edu>
2024-02-03 23:42:00 -0600

    problem6 done

--- property.py
+++ property.py
@@ -1,2 +1,25 @@
 # WAP a program that will demonstrate the @property decorator.
 # [Name of the file should be property.py](1point)
+
+class Student:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+def main():
+    student1 = Student(name="Alice", age=18)
+    student2 = Student(name="Bob", age=17)
+    
+    print(f"Student 1 - Name: {student1.name}, Age: {student1.age}")
+    print(f"Student 2 - Name: {student2.name}, Age: {student2.age}")
+
+if __name__ == "__main__":
+    main()
